fc10bc8c1dd2974930b38ebe8870728741700291
Schokiiiiiiii <fabien@leger-dev.swiss>
2026-04-17 17:34:44 +0200

    dff_ar finished

--- src/dff_ar.vhd
+++ src/dff_ar.vhd
@@ -6,8 +6,8 @@
 --
 -- Description  : 
 -- 
--- Auteur       : Etienne Messerli
--- Date         : 22.10.2014
+-- Auteur       : Fabien Léger
+-- Date         : 17.04.2026
 -- Version      : 0.0
 -- 
 -- Utilise      : Exercice de description d'elements memoire
@@ -15,6 +15,7 @@
 -- 
 --| Modifications |------------------------------------------------------------
 -- Version   Auteur Date               Description
+-- 1.0       FLR    17.04.2026         First version of the flip-flop D
 -- 
 -------------------------------------------------------------------------------
 
@@ -33,17 +34,21 @@ end dff_ar ;
 
 architecture comport of dff_ar is
 
+signal Q_s: std_logic;
 
 begin
 
-
   process(reset_i, clk_i)
   begin
-
-  
+    if reset_i = '1' then
+      Q_s  <= '0';
+    elsif rising_edge(clk_i) then
+      Q_s  <= D_i;
+    end if;
   end process;
 
-
+  Q_o  <= Q_s;
+  nQ_o <= not Q_s;
   
 end comport;
 
