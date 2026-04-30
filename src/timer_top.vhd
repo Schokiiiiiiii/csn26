f8b3edf2ec23c059b4e0fed1bc9e0ccaa7fdd7ad
A2va <49582555+A2va@users.noreply.github.com>
2026-04-30 10:34:35 +0200

    rename cpt_* to reg_*

--- src/timer_top.vhd
+++ src/timer_top.vhd
@@ -42,8 +42,8 @@ end timer_top ;
 
 architecture timer of timer_top is
   signal reset_s  	: std_logic;
-  signal cpt_pres_s : unsigned(6 downto 0);
-  signal cpt_fut_s  : unsigned(6 downto 0);
+  signal reg_pres_s : unsigned(6 downto 0);
+  signal reg_fut_s  : unsigned(6 downto 0);
 
   signal not_enabled_s : std_logic;
   signal eq_zero_s: std_logic;
@@ -55,25 +55,25 @@ begin
     reset_s <= not nReset_i;
 
     not_enabled_s <= '1' when (run_mono_i = '0') and (en_div_i = '0') else '0';
-    eq_zero_s <= '1' when (cpt_pres_s = 0) else
+    eq_zero_s <= '1' when (reg_pres_s = 0) else
                  '0';
 
     load_val_s <= '1' when (eq_zero_s = '1' and en_div_i = '1' and Mono_nDiv_i = '0') else '0';
     load_pres_s <= '1' when (eq_zero_s = '1' and run_mono_i = '1' and Mono_nDiv_i = '1') else '0';
 
 
-    cpt_fut_s <= unsigned(val_i) when not_enabled_s = '1' else
+    reg_fut_s <= unsigned(val_i) when not_enabled_s = '1' else
                  unsigned(val_i) when load_val_s = '1' else
-                 cpt_pres_s when load_pres_s = '1' else
-                 cpt_pres_s - 1;
+                 reg_pres_s when load_pres_s = '1' else
+                 reg_pres_s - 1;
 
 
     process(reset_s, clock_i)
     begin
         if reset_s = '1' then
-            cpt_pres_s <= (others => '0');
+            reg_pres_s <= (others => '0');
         elsif rising_edge(clock_i) then
-            cpt_pres_s <= cpt_fut_s;
+            reg_pres_s <= reg_fut_s;
         end if;
     end process;
 
