7ade3d5b4480d6831b0d7335cddfbc5f2915d735
Schokiiiiiiii <fabien@leger-dev.swiss>
2026-05-02 13:51:51 +0200

    Updated vhdl

--- src/timer_top.vhd
+++ src/timer_top.vhd
@@ -24,6 +24,7 @@
 --                           signal run_mono doit rester actif (voir enonce 2016)
 -- 3.0    ALE   29.04.2026   Ajout du décodeur d'états futur et du registre
 -- 4.0    FLR   01.05.2026   Ajout du décodeur de sorties et réagancement code
+-- 5.0    FLR   02.05.2026   Fix du code après modification table des fct sync
 -------------------------------------------------------------------------------
 
 library ieee;
@@ -49,31 +50,38 @@ architecture timer of timer_top is
   signal cpt_fut_s       : unsigned(6 downto 0);
 
   -- signaux décodeur d'états futurs
-  signal enabled_s       : std_logic;
-  signal load_val_s      : std_logic;
-  signal load_cpt_pres_s : std_logic;
+  signal load_zero_s      : std_logic;
+  signal load_val_minus_s : std_logic;
+  signal load_val_s       : std_logic;
+  signal load_zero_back_s : std_logic; -- même effet que load_zero, mais condition différente
+  signal to_decr_load_s   : unsigned(6 downto 0); -- val ou cpt_pres à décrémenter par la suite
   
   -- signaux décodeur de sorties
   signal eq_zero_s       : std_logic;
-  signal div_nEnDiv_s    : std_logic;
 begin
 
     -- adaptation de polarité
     reset_s <= not nReset_i;
 
     -- sélection décodeur d'états futurs
-    load_val_s      <= '1' when (en_div_i = '0' and run_mono_i = '0') else
-                       '1' when (Mono_nDiv_i = '0' and en_div_i = '0' and run_mono_i = '1') else
-                       '1' when (Mono_nDiv_i = '0' and en_div_i = '1' and eq_zero_s = '1') else -- ajouter load val_i - 1
-                       '1' when (Mono_nDiv_i = '1' and en_div_i = '1' and run_mono_i = '0') else
-                       '0';
-    load_cpt_pres_s <= '1' when (eq_zero_s = '1' and run_mono_i = '1' and Mono_nDiv_i = '1') else
-                       '0';
+    load_zero_s      <= '1' when (Mono_nDiv_i = '0') and (en_div_i = '0') else
+                        '0';
+    load_val_minus_s <= '1' when (Mono_nDiv_i = '0') and (en_div_i = '1') and (eq_zero_s = '1') else
+                        '0';
+    load_val_s       <= '1' when (Mono_nDiv_i = '1') and (run_mono_i = '0') else
+                        '0';
+    load_zero_back_s <= '1' when (Mono_nDiv_i = '1') and (run_mono_i = '1') and (eq_zero_s = '1') else
+                        '0';
+
+    -- décrémentation selon val ou cpt_pres
+    to_decr_load_s <= unsigned(val_i) when load_val_minus_s = '1' else
+                      cpt_pres_s;
 
     -- décodeur d'états futurs
-    cpt_fut_s <= unsigned(val_i) when load_val_s = '1' else
-                 cpt_pres_s when load_cpt_pres_s = '1' else
-                 cpt_pres_s - 1;
+    cpt_fut_s <= (others => '0') when (load_zero_s = '1') or (load_zero_back_s = '1') else
+                 unsigned(val_i) when (load_val_s = '1') else
+                 to_decr_load_s - 1;
+                 
 
     -- registre 
     process(reset_s, clock_i)
@@ -87,11 +95,10 @@ begin
 
     -- décodeur de sorties
     -- égalité à '0' -> fin du timer
-    eq_zero_s    <= '1' when (cpt_pres_s = 0) else
-    	            '0';
+    eq_zero_s <= '1' when (cpt_pres_s = 0) else
+    	         '0';
     	            
     -- mise à '1' si en mod div sans l'en activé sinon égalité avec zéro
-    done_o <= '1' when ((Mono_nDiv_i = '1' nor en_div_i = '1') and cpt_pres_s = unsigned(val_i)) else
-              eq_zero_s;
+    done_o <= eq_zero_s;
 
 end timer;
