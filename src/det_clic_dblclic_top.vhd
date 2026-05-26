58b7162d423b6c852dcc9d1909e9b07ec75b1407
Schokiiiiiiii <fabien@leger-dev.swiss>
2026-05-26 08:34:48 +0200

    Fixed code top

--- src/det_clic_dblclic_top.vhd
+++ src/det_clic_dblclic_top.vhd
@@ -10,9 +10,10 @@
 -- 
 -- Utilise      : Labo SysLog2 2016
 --| Modifications |------------------------------------------------------------
--- Ver   Date        Qui         Description
+-- Ver   Date        Qui   Description
 -- 1.0   20.11.2020  EMI   Ajout generique pour timer et maintien
--- 
+-- 2.0   25.05.2026  JNI   Added entities and code structure
+-- 2.1   26.05.2026  FLR   Improved readability and removed useless declarations
 -------------------------------------------------------------------------------
 
 library ieee;
@@ -48,128 +49,107 @@ architecture struct of det_clic_dblclic_top is
   signal simple_click_s : std_logic;
   signal double_click_s : std_logic;
 
-    ----------
-    -- TODO --
-    ----------
-
   
-   -- Component declarations
-   
-   
-    ----------
-    -- TODO --
-    ----------
-   
-	component timer
-		generic (
-        T1_g : natural range 1 to 1023 := 300;
-        T2_g : natural range 1 to 1023 := 200 );	
-      port (
-        clock_i    : in  std_logic;
-        reset_i    : in  std_logic;
-        start_i    : in  std_logic;
-        top_ms_i   : in  std_logic;
-        trigger1_o : out std_logic;
-        trigger2_o : out std_logic
-        );
-   end component;
-	for all : timer use entity work.timer;
+  -- Component declarations
+  component timer
+    generic (
+             T1_g : natural range 1 to 1023 := 300;
+             T2_g : natural range 1 to 1023 := 200
+             );	
+    port (
+          clock_i    : in  std_logic;
+          reset_i    : in  std_logic;
+          start_i    : in  std_logic;
+          top_ms_i   : in  std_logic;
+          trigger1_o : out std_logic;
+          trigger2_o : out std_logic
+          );
+  end component;
+  for all : timer use entity work.timer;
 	
-	component mss_clic_dblclic
-		port (
-        clock_i        : in  std_logic;
-        reset_i        : in  std_logic;
-        trigger1_i     : in  std_logic;
-        trigger2_i     : in  std_logic;
-        bouton_i       : in  std_logic;
-		  top_ms_i       : in  std_logic;
-        start_o        : out std_logic;
-		  simple_click_o : out std_logic;
-		  double_click_o : out std_logic
-        );
-   end component;
-	for all : mss_clic_dblclic use entity work.mss_clic_dblclic;
+  component mss_clic_dblclic
+    port (
+          clock_i        : in  std_logic;
+          reset_i        : in  std_logic;
+          trigger1_i     : in  std_logic;
+          trigger2_i     : in  std_logic;
+          bouton_i       : in  std_logic;
+          start_o        : out std_logic;
+          simple_click_o : out std_logic;
+	  double_click_o : out std_logic
+          );
+  end component;
+  for all : mss_clic_dblclic use entity work.mss_clic_dblclic;
 
-    component maintien
-        generic (T_HOLD : natural range 1 to 1023 := 2
-                );
-        port (clock_i    : in  std_logic;
-              reset_i    : in  std_logic;
-              pulse_i    : in  std_logic;
-              top_ms_i   : in  std_logic;
-              p_hold_o   : out std_logic
-              );
-    end component;
-    for all : maintien use entity work.maintien;
+  component maintien
+    generic (T_HOLD : natural range 1 to 1023 := 2
+             );
+    port (clock_i    : in  std_logic;
+          reset_i    : in  std_logic;
+          pulse_i    : in  std_logic;
+          top_ms_i   : in  std_logic;
+          p_hold_o   : out std_logic
+          );
+  end component;
+  for all : maintien use entity work.maintien;
 
 
 begin
 	
-	reset_s <= not nReset_i;
+  reset_s <= not nReset_i;
 	
-	process(clock_i, button_i)
-	begin
-		if rising_edge(clock_i) then
-			button_s <= button_i;
-		end if;
-	end process;
+  process(clock_i, button_i)
+  begin
+    if rising_edge(clock_i) then
+      button_s <= button_i;
+    end if;
+  end process;
 		
 
-	U_timer : timer
-		generic map (
-			T1_g => T1_c,
-			T2_g => T2_c
-			
-		)
-		port map (
-			clock_i => clock_i,
-			reset_i => reset_s,
-			start_i => start_s,
-			top_ms_i => top_ms_i,
-			trigger1_o => trigger1_s,
-			trigger2_o => trigger2_s
-		);
+  U_timer : timer
+    generic map (T1_g => T1_c,
+		 T2_g => T2_c
+		 )
+    port map (clock_i => clock_i,
+	      reset_i => reset_s,
+	      start_i => start_s,
+	      top_ms_i => top_ms_i,
+	      trigger1_o => trigger1_s,
+	      trigger2_o => trigger2_s
+	      );
 		
-	mss : mss_clic_dblclic
-		port map(
-		  clock_i        => clock_i,
-        reset_i        => reset_s,
-        trigger1_i     => trigger1_s,
-        trigger2_i     => trigger2_s,
-        bouton_i       => button_s,
-		  top_ms_i       => top_ms_i,
-        start_o        => start_s,
-		  simple_click_o => simple_click_s,
-		  double_click_o => double_click_s
-		);
+  mss : mss_clic_dblclic
+    port map(clock_i        => clock_i,
+             reset_i        => reset_s,
+             trigger1_i     => trigger1_s,
+             trigger2_i     => trigger2_s,
+             bouton_i       => button_s,
+             start_o        => start_s,
+             simple_click_o => simple_click_s,
+             double_click_o => double_click_s
+	     );
 	
-	clic_lg : maintien
-		generic map (
-		  T_HOLD => T_HOLD_C
+  clic_lg : maintien
+    generic map (T_HOLD => T_HOLD_C
 		)
-		port map (
-		  clock_i    => clock_i,
-        reset_i    => reset_s,
-        pulse_i    => simple_click_s,
-        top_ms_i   => top_ms_i,
-        p_hold_o   => clic_lg_o
-		);
+    port map (clock_i    => clock_i,
+              reset_i    => reset_s,
+              pulse_i    => simple_click_s,
+              top_ms_i   => top_ms_i,
+              p_hold_o   => clic_lg_o
+	      );
 		
-	dbl_clic_lg : maintien
-		generic map (
-		  T_HOLD => T_HOLD_C
-		)
-		port map (
-		  clock_i    => clock_i,
-        reset_i    => reset_s,
-        pulse_i    => double_click_s,
-        top_ms_i   => top_ms_i,
-        p_hold_o   => dbl_clic_lg_o
-		);
+  dbl_clic_lg : maintien
+    generic map (T_HOLD => T_HOLD_C
+		 )
+    port map (clock_i    => clock_i,
+              reset_i    => reset_s,
+              pulse_i    => double_click_s,
+              top_ms_i   => top_ms_i,
+              p_hold_o   => dbl_clic_lg_o
+	      );
 	
-		clic_o <= simple_click_s;
-		dbl_clic_o <= double_click_s;
-
-
+  clic_o <= simple_click_s;
+  dbl_clic_o <= double_click_s;
 
 end struct;
